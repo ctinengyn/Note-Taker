7058d5480d8293580989935c3189eccbf44dd5f3
Christine Nguyen <ctine.ngyn@gmail.com>
2021-04-26 12:54:22 -0700

    Comments for htmlRoutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,10 +2,12 @@ const path = require("path");
 
 module.exports = function(app){
 
+    // Route to notes page
     app.get("/notes", function(req, res){
         res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
 
+    // Route to index page
     app.get("/", function(req, res){
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
