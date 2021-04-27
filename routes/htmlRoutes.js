0b8c7f670b4e5c8f3a8111e82e28e68a7b9a53e2
Christine Nguyen <ctine.ngyn@gmail.com>
2021-04-26 17:01:10 -0700

    More commits for htmlRoutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,12 @@
+// Dependencies
 const path = require("path");
 
+// Routing
 module.exports = function(app){
+
+    // HTML GET Requests
+    // When users "visit" page
+    // Each cases the user is shown an HTML page
 
     // Route to notes page
     app.get("/notes", function(req, res){
