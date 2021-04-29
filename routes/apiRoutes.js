030a4f22b538de610f89a49c4729afb1715544b9
Christine Nguyen <ctine.ngyn@gmail.com>
2021-04-29 00:35:54 -0700

    Update apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,24 @@
 const fs = require("fs");
-const notesData = require("../db/db.json");
 
 module.exports = function(app){
+    let notes = require("../db/db.json");
 
-    
+    app.get("/api/notes", (req, res)=>{
+        
+        return res.json(notes)
+    })
+
+    app.get("/api/notes/:id", (req, res) => {
+
+        const id = req.params.id;
+        let found;
+        notes.forEach(n => {
+            if (id == n.id){
+                found = n;
+                return res.json(n)
+            }
+        })
+        return res.json(false)
+    })
+
 }
